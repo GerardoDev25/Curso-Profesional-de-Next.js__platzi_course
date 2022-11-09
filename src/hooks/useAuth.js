fca0309d6c44f21ded48b2123b90435596f68006
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-09 12:09:26 -0400

    Guardado del token en una cookie para mantener la sesión

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -24,14 +24,17 @@ function useProvideAuth() {
         "Content-Type": "application/json",
       },
     };
-    const { data: access_token, status } = await axios.post(
+    const { data: access_token } = await axios.post(
       endPoints.auth.login,
       { email, password },
       options
     );
-    console.log(status);
     if (access_token) {
-      Cookie.set("token", access_token.access_token, { expires: 5 });
+      const token = access_token.access_token;
+      Cookie.set("token", token, { expires: 5 });
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      setUser(user);
     }
   };
 
