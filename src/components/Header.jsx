fe953c9633d96672827d2a9aec0cb463a6b24a89
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-10 20:40:45 -0400

    Implementación del logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,12 @@ import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
-  { name: "Productos", href: "/dashboard/products/", current: false },
+  { name: "Productos", href: "/dashboard/product/", current: false },
   { name: "Ventas", href: "#", current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -94,21 +93,12 @@ export default function Header() {
                         leaveTo="transform opacity-0 scale-95"
                       >
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a
-                                  href={item.href}
-                                  className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
-                                  )}
-                                >
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
+                          <button
+                            onClick={() => auth.logout()}
+                            className="block px-4 py-2 text-sm text-gray-700"
+                          >
+                            Logout
+                          </button>
                         </Menu.Items>
                       </Transition>
                     </Menu>
